fb3a82bd064086688de8ac9bdce40c9ebfd780f9
ryansmane <ryanzimmer8@gmail.com>
2020-02-14 10:32:18 -0500

    front-end-started

---
+++ moviefrontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import HeaderContainer from './components/HeaderContainer';
+import Home from './components/Home';
+import Movie from './components/Movie';
+import User from './components/User';
+import Create from './components/Create';
+import { Route, Link, Switch } from 'react-router-dom';
+import './App.css';
+
+const App = () => {
+   return (
+      <div>
+         <div>
+            <HeaderContainer />
+         </div>
+         <main>
+            <Switch>
+               <Route exact path='/' component={Home} />
+               <Route exact path='/new' component={Create} />
+               <Movie />
+               <User />
+            </Switch>
+         </main>
+      </div>
+   );
+};
+
+export default App;
---
+++ moviefrontend/src/components/HeaderContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom'
+
+const HeaderContainer = props => {
+   return (
+      <div>
+         <nav className='nav-bar'>
+            <h3>Movie App</h3>
+               <Link to='/'><h3>Home</h3></Link>
+               <Link to='/new'><h3>Submit</h3></Link>
+         </nav>
+      </div>
+   );
+};
+
+export default HeaderContainer;
