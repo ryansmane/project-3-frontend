a998d082522e8b3954ea53ba0618e01e8962014c
SuzannAdams <suzannyee@yahoo.com>
2020-02-14 09:58:26 -0800

    set up login component

--- moviefrontend/src/components/Login.js
+++ moviefrontend/src/components/Login.js
@@ -2,27 +2,56 @@ import React from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState, useEffect } from 'react';
 
 const Login = props => {
-   return (
-      <div>
-         <Form className='login-form'>
-            <Form.Group controlId='formBasicEmail'>
-               <Form.Label>Email address</Form.Label>
-               <Form.Control type='email' placeholder='Enter email' />
-               <Form.Text className='text-muted'></Form.Text>
-            </Form.Group>
+  const [credentials, setCredentials] = useState({});
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-            <Form.Group controlId='formBasicPassword'>
-               <Form.Label>Password</Form.Label>
-               <Form.Control type='password' placeholder='Password' />
-            </Form.Group>
-            <Button variant='primary' type='submit'>
-               Submit
-            </Button>
-         </Form>
-      </div>
-   );
+  useEffect(() => {
+    const info = { email, password };
+    setCredentials(info);
+  }, [password]);
+
+  const getInfo = () => {
+    const info = { email, password };
+    setCredentials(info);
+  };
+
+  return (
+    <div>
+      <Form className="login-form">
+        <Form.Group
+          onChange={evt => setEmail(evt.target.value)}
+          controlId="formBasicEmail"
+        >
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+
+        <Form.Group
+          onChange={evt => setPassword(evt.target.value)}
+          controlId="formBasicPassword"
+        >
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button
+          onClick={evt => {
+            evt.preventDefault();
+            getInfo();
+            console.log(credentials);
+          }}
+          variant="primary"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Login;
