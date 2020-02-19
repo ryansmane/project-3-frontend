a33eabaf8a6f7ace8d3192d29008fa95d15a1d6f
ryansmane <ryanzimmer8@gmail.com>
2020-02-19 11:55:26 -0500

    Updates

---
+++ precache-manifest.d518b7e99f81e5766e2dda8682c35c00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4abf971e2a59781c4fb753b02a7bbcd",
+    "url": "/project-3-frontend/index.html"
+  },
+  {
+    "revision": "72d9ba95dc9f6c586056",
+    "url": "/project-3-frontend/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "04b7ae463731e42dc8e1",
+    "url": "/project-3-frontend/static/css/main.7cf24212.chunk.css"
+  },
+  {
+    "revision": "72d9ba95dc9f6c586056",
+    "url": "/project-3-frontend/static/js/2.7563421f.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/project-3-frontend/static/js/2.7563421f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04b7ae463731e42dc8e1",
+    "url": "/project-3-frontend/static/js/main.9fb484de.chunk.js"
+  },
+  {
+    "revision": "362c8e09bb0f74611bf4",
+    "url": "/project-3-frontend/static/js/runtime-main.1c8e83d2.js"
+  }
+]);
